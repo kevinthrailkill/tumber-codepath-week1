370f6ac37e40a21dc7d9ae78ad1ddcaa86899f48
Kevin Thrailkill <kevinjt92@gmail.com>
2017-03-29 11:31:50 -0700

    Initial Commit

---
+++ tumblr feed/tumblr feed/FeedPost.swift
@@ -0,0 +1,17 @@
+//
+//  FeedPost.swift
+//  tumblr feed
+//
+//  Created by Kevin Thrailkill on 3/29/17.
+//  Copyright © 2017 kevinthrailkill. All rights reserved.
+//
+
+import Foundation
+
+
+class FeedPost {
+    
+    
+    
+    
+}
---
+++ tumblr feed/tumblr feed/TumblrNetworkLayer.swift
@@ -0,0 +1,28 @@
+//
+//  TumblrNetworkLayer.swift
+//  tumblr feed
+//
+//  Created by Kevin Thrailkill on 3/29/17.
+//  Copyright © 2017 kevinthrailkill. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class TumblerNetworkLayer {
+    
+    class func getTumberFeed() -> [FeedPost] {
+        
+        var posts: [FeedPost] = []
+        
+        
+        
+        
+        
+        
+        return posts
+        
+        
+    }
+    
+}
